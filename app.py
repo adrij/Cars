4ffbd85fcef80454fb5fdff5ad55ef7ed02b0ad7
Adribadri <juhasz.adrienn.kitti@gmail.com>
2019-06-21 09:42:45 +0200

    SelectField for cartypes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, request
 from forms import CarsForm
 import pickle
 from cars_functions import calc_price
@@ -11,16 +11,21 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 @app.route("/", methods=['GET', 'POST'])
+@app.route("/cars", methods=['GET', 'POST'])
 def cars():
     form = CarsForm() ######CarsForm()
     if form.validate_on_submit():
-        #price = calc_price(scaler_model, model, {form.cartype.data},{form.age_in_years.data},
-        	#{form.performance_kw.data}, {form.speedometer.data},{form.weightTotal.data},
-        	#{form.capacity.data}, {form.flag_back_w.data},{form.luggagerack.data})
-        price = calc_price(scaler_model, model,'OPEL CORSA', 10000, #{form.cartype.data},{form.age_in_years.data}
-        	200,10000,400,#{form.performance_kw.data}, {form.speedometer.data},{form.weightTotal.data},
-        	500, 0, 599)#{form.capacity.data}, {form.flag_back_w.data},{form.luggagerack.data})
+        try :
+        	temp=request.form['flag_back_w']
+        	flag_bw=1
+        except:
+        	flag_bw=0
+        price = calc_price(scaler_model, model, request.form['cartype'], request.form['age_in_years'], 
+        	request.form['performance_kw'], request.form['speedometer'], request.form['weightTotal'],
+        	request.form['capacity'], flag_bw, request.form['luggagerack'])
+        
         flash('The average price of the car is {:.2f}'.format(price[0]), 'success')
+        #return redirect(url_for('cars'))
     return render_template('cars.html', title='Cars', form=form)
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,16 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField, IntegerField
+from wtforms import StringField, SubmitField, BooleanField, IntegerField, SelectField
 from wtforms.validators import DataRequired
+import pandas as pd
 
 
 class CarsForm(FlaskForm):
-    cartype = StringField('Type of the car', validators=[DataRequired()])
+    df_avgprice=pd.read_csv('df_avgprice.csv')
+    subset = df_avgprice.iloc[:,1]
+    tuples_list = list(zip(subset, subset)) 
+    #tuples_list = [tuple(x) for x in subset.values]
+    #n=len(subset)
+    #tuples_list= [(str(x), df_avgprice.iloc[x,1]) for x in range(n)]
+
+    cartype = SelectField('Type of the car', choices=tuples_list)
     age_in_years = IntegerField('Age in years',
                            validators=[DataRequired()])
     performance_kw = IntegerField('Performance in kW',
                         validators=[DataRequired()])
     speedometer = IntegerField('speedometer', validators=[DataRequired()])
-    weightTotal = IntegerField('Weight', validators=[DataRequired()])
+    weightTotal = IntegerField('Weight total', validators=[DataRequired()])
     capacity = IntegerField('Capacity', validators=[DataRequired()])
     luggagerack = IntegerField('Luggagerack', validators=[DataRequired()])
     flag_back_w = BooleanField('Back wheel prop')
